bbc058ee00a069a6b7f10cb856f6703841e3e748
Kavish Rizvi <rizvikavish205.com>
2021-09-19 18:43:26 +0530

    Added resttemplate code

--- src/main/java/com/caching/example/microservicescaching/service/UserDetailsImpl.java
+++ src/main/java/com/caching/example/microservicescaching/service/UserDetailsImpl.java
@@ -1,11 +1,23 @@
 package com.caching.example.microservicescaching.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class UserDetailsImpl implements UserDetails{
+    @Autowired
+    private RestTemplate template;
     @Override
     public String getUserDetails() throws Exception {
+        template.exchange("https://reqres.in/api/users", HttpMethod.GET, getHttpEntity(), String.class);
         return null;
+    }
+
+    private HttpEntity getHttpEntity() {
+        HttpEntity<> entity = new HttpEntity();
+        return entity;
     }
 }
