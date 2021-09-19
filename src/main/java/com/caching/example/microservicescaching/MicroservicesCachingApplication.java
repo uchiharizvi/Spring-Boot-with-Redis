ea4ef1695f94fdc54723281fee1c51e2e74a82fc
Kavish Rizvi <rizvikavish205.com>
2021-09-19 18:06:21 +0530

    Added Code

---
+++ src/main/java/com/caching/example/microservicescaching/MicroservicesCachingApplication.java
@@ -0,0 +1,13 @@
+package com.caching.example.microservicescaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesCachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesCachingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/caching/example/microservicescaching/MicroservicesCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caching.example.microservicescaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesCachingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
