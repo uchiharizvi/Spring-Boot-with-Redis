d970e606e583c4009268d1b80b6b57088fba5073
Kavish Rizvi <rizvikavish205@gmail.com>
2021-09-20 05:52:31 +0530

    added config details

--- src/main/java/com/caching/example/microservicescaching/MicroservicesCachingApplication.java
+++ src/main/java/com/caching/example/microservicescaching/MicroservicesCachingApplication.java
@@ -2,8 +2,10 @@ package com.caching.example.microservicescaching;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class MicroservicesCachingApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/caching/example/microservicescaching/dao/UserDetails.java
@@ -0,0 +1,9 @@
+package com.caching.example.microservicescaching.dao;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+@Entity
+
+public class UserDetails implements Serializable {
+}
